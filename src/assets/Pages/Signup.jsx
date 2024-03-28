463a8fb03692315a9a6da949938074756ecc2d4a
PARAISO NELSON <NELSONITO@air-de-paraiso.home>
2024-03-28 17:20:21 +0100

    modified signup

--- src/assets/Pages/Signup.jsx
+++ src/assets/Pages/Signup.jsx
@@ -2,26 +2,28 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const Signup = () => {
-    const [userName, setUserName] = useState("");
+    const [username, setUserName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [newsLetter, setNewsLetter] = useState(true);
+    const [newsletter, setNewsLetter] = useState(true);
+
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-
-
         try {
             const response = await axios.post('https://lereacteur-vinted-api.herokuapp.com/user/signup', {
+
                 email: email,
-                userName: userName,
+                username: username,
                 password: password,
-                newsLetter: newsLetter,
+                newsletter: newsletter,
             });
             //console.log(response);
             const token = response.data.token;
+
         } catch (error) {
             console.log(error.response.data);
         }
@@ -35,15 +37,13 @@ const Signup = () => {
 
                 <h2>S'inscrire</h2>
 
-                <input type="text" placeholder="Nom de l'utilisateur" value={userName} onChange={(event) => setUserName(event.target.value)} />
+                <input type="text" placeholder="Nom de l'utilisateur" value={username} onChange={(event) => setUserName(event.target.value)} />
 
                 <input type="email" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)} />
 
                 <input type="password" placeholder="Mot de passe" value={password} onChange={(event) => setPassword(event.target.value)} />
 
-                <input type="password" placeholder="Verification de votre mot de passe" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
-
-                <input type="checkbox" checked={newsLetter} onChange={(event) => setNewsLetter(event.target.checked)} />
+                <input type="checkbox" checked={newsletter} onChange={(event) => setNewsLetter(event.target.checked)} />
 
                 <h3>S'inscrire à notre newsletter</h3>
                 <p>En m'inscrivant je confirme voir lu et accepté les Termes & Conditions et politique de Confidentialité de Vinted.je confirme avoir au moins 18 ans.</p>
